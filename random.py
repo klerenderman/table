92ba1c8ddf2abbcb7ee4a7bc27031c040e3ba0bf
klerenderman <104389011+klerenderman@users.noreply.github.com>
2024-02-22 16:31:32 -0700

    Update random.py
    .png export function

--- random.py
+++ random.py
@@ -34,11 +34,6 @@ y_pred = clf.predict(X_test)
 # Model Accuracy, how often is the classifier correct?
 print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
 
-from sklearn.tree import export_graphviz
-from sklearn.externals.six import StringIO  
-from IPython.display import Image  
-import pydotplus
-
 dot_data = StringIO()
 export_graphviz(clf, out_file=dot_data,  
                 filled=True, rounded=True,
@@ -49,7 +44,6 @@ Image(graph.create_png())
 =======
 #tools
 import numpy as np
-import pandas as pd
 
 #getting data
 data = pd.read_csv("airfoil_noise_data.csv")
@@ -200,5 +194,17 @@ class DecisionTreeRegressor():
 X = data.iloc[:, :-1].values
 Y = data.iloc[:, -1].values.reshape(-1,1)
 
+from sklearn.tree import export_graphviz
+from sklearn.externals.six import StringIO  
+from IPython.display import Image  
+import pydotplus
+
+dot_data = StringIO()
+export_graphviz(clf, out_file=dot_data,  
+                filled=True, rounded=True,
+                special_characters=True,feature_names = feature_cols,class_names=['0','1'])
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+graph.write_png('diabetes.png')
+Image(graph.create_png())
 
 #>>>>>>> 032b61f39a30b8782d007682241d8ac6a15b3fd2
